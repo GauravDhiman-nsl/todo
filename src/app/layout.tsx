35f1e6e6be628112608929e39fa210e297a57777
GauravDhiman-nsl <gaurav.dhiman@netsolutions.com>
2025-08-26 16:13:28 +0530

    Tweaks
    Component-readonly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" className="h-full">
       <body className={`${geistSans.variable} min-h-dvh bg-white text-gray-900 antialiased`}>
